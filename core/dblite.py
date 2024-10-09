52dbe1bd4a70c7e5894a8bfe326540cbf8733298
Santos HL <shernanz@mapa.es>
2024-10-09 17:42:30 +0200

    entrecomillar nombre de tabla en los insert

--- core/dblite.py
+++ core/dblite.py
@@ -396,7 +396,7 @@ class DBLite:
                 if values:
                     yield val_to_str(values, ";")
                     values = []
-                yield f"INSERT INTO {table} VALUES"
+                yield f'INSERT INTO "{table}" VALUES'
                 count = multiple_limit
             values.append("("+m.group(2)+")")
             if len(values) > 1 and len(",".join(values)) > width_values:
